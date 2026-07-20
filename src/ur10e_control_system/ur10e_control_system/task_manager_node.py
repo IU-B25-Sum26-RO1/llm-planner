8f03219bdcf747782ba43cc21a695810670cf599
Aioma <idfc1200@gmail.com>
2026-07-20 14:34:01 +0300

    Add: grippep command processing

--- src/ur10e_control_system/ur10e_control_system/task_manager_node.py
+++ src/ur10e_control_system/ur10e_control_system/task_manager_node.py
@@ -7,10 +7,12 @@ import queue
 import rclpy                                       # type: ignore
 from rclpy.node import Node                        # type: ignore
 from rclpy.action import ActionClient              # type: ignore
+from rclpy.executors import MultiThreadedExecutor  # type: ignore
 from std_msgs.msg import String                    # type: ignore
 from sensor_msgs.msg import Image, CompressedImage # type: ignore
 
-from robot_interfaces import BaseAction            # type: ignore
+from robot_interfaces.action import BaseAction     # type: ignore
+from robot_interfaces.srv import GripperControl    # type: ignore
 
 class TaskManagerNode(Node):
     def __init__(self):
@@ -34,6 +36,11 @@ class TaskManagerNode(Node):
             self, BaseAction, base_action_topic 
         )
 
+        self.gripper_client = self.create_client(
+            GripperControl,
+            gripper_control_topic
+        )
+
         self.loop = None
         self.loop_tread = threading.Thread(target=self._run_async_loop, daemon=True)
         self.loop_tread.start()
@@ -50,39 +57,84 @@ class TaskManagerNode(Node):
             self.get_logger().info(f"Async loop closed.")
     
     async def orchestrator(self):
-        self.loop = asyncio.get_running_loop()
         self.task_queue = asyncio.PriorityQueue(maxsize=10)
 
+        self.get_logger().info("Task Manager | Waiting for Action Server...")
+        await asyncio.to_thread(self.action_client.wait_for_server)
+        self.get_logger().info("Task Manager | Action Server is ready!")
+
+        self.get_logger().info("Task Manager | Waiting for Gripper Service...")
+        await asyncio.to_thread(self.gripper_client.wait_for_service)
+        self.get_logger().info("Task Manager | Gripper Service is ready!")
+
         while rclpy.ok():
-            self.action_client.wait_for_server()
-            task = await self.task_queue.get()
-            success = await self.send_task_to_robot(task)
+            try:
+                items = await self.task_queue.get()
+                task = items[2]
+                action = task["action"]
+                success = False
+                
+                if action == "open_gripper":
+                    success = await self.send_gripper_command(activate=False)
+                elif action == "close_gripper":
+                    success = await self.send_gripper_command(activate=True)
+                else:
+                    success = await self.send_task_to_robot(task)
+            
+                if success:
+                    self.get_logger().info("Task Manager | Task successfully completed")
+                else:
+                    self.get_logger().warn("Task Manager | Task failed")
+                
+                self.task_queue.task_done()
+            except asyncio.CancelledError:
+                break
+            except Exception as e:
+                self.get_logger().error(f"Task Manager | Error in orchestrator loop: {e}")
+    
+    async def send_gripper_command(self, activate: bool) -> bool:
+        try:
+            self.get_logger().info(f"Task Manager | Received gripper command: {'close' if activate else 'open'}")
+            request = GripperControl.Request()
+            request.activate = activate
+
+            srv_future = self.gripper_client.call_async(request)
+
+            response = await self._async_ros_future(srv_future)
+
+            return response.success
         
-            if success:
-                self.get_logger().info("Task Manager | Task successfully completed")
-            else:
-                self.get_logger().warn("Task Manager | Task failed")
+        except Exception as e:
+            self.get_logger().error(f"Task Manager | Error while sending gripper command: {str(e)}")
+            return False
 
     async def send_task_to_robot(self, task):
         try:
+            self.get_logger().info(f"Task Manager | Received task: {task['id']} ({task['action']})")
             goal_msg = self.create_goal_msg(task)
 
-            send_goal_future = await self.action_client.send_goal_async(goal_msg)
-            goal_handle = send_goal_future
+            send_goal_future = self.action_client.send_goal_async(goal_msg)
+            
+            goal_handle = await self._async_ros_future(send_goal_future)
 
             if not goal_handle.accepted:
-                self.get_logger().error(f"TaskM Manager | Robot rejected the task")
+                self.get_logger().error("Task Manager | Robot rejected the task")
                 return False
                         
-            self.get_logger().info(f"Task Manager | Robot accepted the task. Waiting for result")
+            self.get_logger().info("Task Manager | Robot accepted the task. Waiting for result...")
 
-            get_result_future = await goal_handle.get_result_async()
-            result = get_result_future.result
+            get_result_future = goal_handle.get_result_async()
+            result_response = await self._async_ros_future(get_result_future)
 
-            return result.success
+            return result_response.result.success
         
         except Exception as e:
-            self.get_logger().info(f"Error while sending task: {str(e)}")
+            self.get_logger().error(f"Error while sending task: {str(e)}")
+            return False
+
+    def _wait_for_rclpy_future(self, rclpy_future):
+        rclpy.spin_until_future_complete(self, rclpy_future)
+        return rclpy_future.result()
     
     def create_goal_msg(self, task: dict):
         goal_msg = BaseAction.Goal()
@@ -92,10 +144,30 @@ class TaskManagerNode(Node):
         goal_msg.z = 0.0
 
         goal_msg.task_type = task["action"]
-        goal_msg.object_name = task["target"]["object"]["class"] or ""
+        task_type = task["action"]
+
+        if task_type == "place":
+            object_name = "_".join(task["placement"]["reference"]["object"]["prompt"].split())
+        elif task_type == "pick":
+            object_name = "_".join(task["target"]["object"]["prompt"].split())
+        else: 
+            object_name = ""
+        goal_msg.object_name = object_name
 
         return goal_msg
+    
+    async def _async_ros_future(self, rclpy_future):
+        """Конвертирует rclpy.task.Future в честный asyncio.Future, 
+        который не блокирует Executor ноды."""
+        loop = asyncio.get_running_loop()
+        asyncio_future = loop.create_future()
 
+        def cb(fut):
+            if not asyncio_future.done():
+                loop.call_soon_threadsafe(asyncio_future.set_result, fut.result())
+
+        rclpy_future.add_done_callback(cb)
+        return await asyncio_future
     
     def _command_callback(self, msg: String) -> None:
         if self.task_queue is None:
@@ -107,7 +179,7 @@ class TaskManagerNode(Node):
             if cmd_obj['type'] == 'non_command' or cmd_obj['confidence'] < 0.5: 
                 return
             
-            self.get_logger().info(f'Manager received new command: {cmd_obj['text']}')
+            self.get_logger().info(f"Manager received new command: {cmd_obj['text']}")
 
             for task in cmd_obj["tasks"]:
                 priority = 0 if task["action"] in ("stop", "cancel") else 1
@@ -120,21 +192,20 @@ class TaskManagerNode(Node):
         except Exception as e:
             self.get_logger().error(f"Task Manager | Error in command_callback: {e}")
 
-    def _create_task(self, action, **args) -> dict:
-        if action == 'pick':
-            pass
-
-        return {
-            'action': action,
-        }
 
 def main(args=None):
     rclpy.init(args=args)
     node = TaskManagerNode()
+
+    executor = MultiThreadedExecutor()
+    executor.add_node(node)
     try:
-        rclpy.spin(node)
+        executor.spin()
     except KeyboardInterrupt:
         pass
     finally:
         node.destroy_node()
         rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
